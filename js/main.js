45b9a081fe066cef9d2fa60d310dc905cb8d9456
Richard Strauss <rstrauss91@gmail.com>
2017-02-15 17:06:42 -0500

    create jQuery functionality

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
+$(function() {
+  $('.status').html('Test')});
 
-
+$('button').on('click', function() {
+  if ($('button').attr('class') === 'on') {
+    $('button').attr('class','off');
+    $('body').attr('class','dark');
+    $('.status').html("Who turned off the lights!")}
+  else {
+    $('button').attr('class','on');
+    $('body').attr('class','light');
+    $('.status').html("It's so bright in here!")}
+  });
